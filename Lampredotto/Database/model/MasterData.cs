17d2fc6054d5f7276423cfc8ef334ae7173544cc
Ongaku96 <marco.biagini96@gmail.com>
2022-02-23 01:16:54 +0100

    Edit MasterData

--- Lampredotto/Database/model/MasterData.cs
+++ Lampredotto/Database/model/MasterData.cs
@@ -10,8 +10,8 @@ namespace Lampredotto.Database.model
     public class MasterData : IDataModel
     {
         public int id { get; set; } = 0;
-        public string nome { get; set; } = "";
-        public string note { get; set; } = "";
+        public string name { get; set; } = "";
+        public string description { get; set; } = "";
         public int last_user { get; set; } = 0;
         public int owner { get; set; } = 0;
         public DateTime time_stamp { get; set; } = DateTime.MaxValue;
@@ -22,21 +22,22 @@ namespace Lampredotto.Database.model
         protected override void SetFields()
         {
             fields.Add(Campi.id, "ID");
-            fields.Add(Campi.nome, "Nome");
-            fields.Add(Campi.note, "Note");
+            fields.Add(Campi.name, "Name");
+            fields.Add(Campi.description, "Description");
             fields.Add(Campi.last_user, "IDLastUser");
             fields.Add(Campi.owner, "IDOwner");
             fields.Add(Campi.time_stamp, "TimeStamp");
+            fields.Add(Campi.insert, "TimeStamp");
         }
         public override void BuildModel(DataRow _data)
         {
             id = ModelElaboration.ReadDbData(_data, GetField(Campi.id), 0);
-            nome = ModelElaboration.ReadDbData(_data, GetField(Campi.nome), "");
-            note = ModelElaboration.ReadDbData(_data, GetField(Campi.note), "");
+            name = ModelElaboration.ReadDbData(_data, GetField(Campi.name), "");
+            description = ModelElaboration.ReadDbData(_data, GetField(Campi.description), "");
             last_user = ModelElaboration.ReadDbData(_data, GetField(Campi.last_user), 0);
             owner = ModelElaboration.ReadDbData(_data, GetField(Campi.owner), 0);
             time_stamp = ModelElaboration.ReadDbData(_data, GetField(Campi.time_stamp), DateTime.MaxValue);
-            insert = ModelElaboration.ReadDbData(_data, "InsertTime", DateTime.MaxValue);
+            insert = ModelElaboration.ReadDbData(_data, GetField(Campi.insert), DateTime.MaxValue);
         }
         public virtual void SetUser(int _id)
         {
@@ -45,18 +46,18 @@ namespace Lampredotto.Database.model
         }
         protected override void SetIdentity()
         {
-            identity.Add(Campi.insert);
+            identity.Add(Campi.id);
         }
         protected override void SetPrimaryKeys()
         {
-            throw new NotImplementedException();
+            primary_keys.Add(Campi.id);
         }
 
         public class Campi
         {
             public const string id = "id";
-            public const string nome = "nome";
-            public const string note = "note";
+            public const string name = "name";
+            public const string description = "description";
             public const string last_user = "last_user";
             public const string owner = "owner";
             public const string time_stamp = "time_stamp";
